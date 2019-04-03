1ce94b5e63b5372fe72c3b8ad056cddd6edda975
nishantsir57 <38082502+nishantsir57@users.noreply.github.com>
2019-04-03 11:34:13 +0530

    BinarySearchClass.java

---
+++ BinarySearchClass.java
@@ -0,0 +1,20 @@
+static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n; ++i) 
+            System.out.print(arr[i] + " "); 
+  
+        System.out.println(); 
+    } 
+  
+    // Driver method 
+    public static void main(String args[]) 
+    { 
+        int arr[] = { 12, 11, 13, 5, 6 }; 
+  
+        InsertionSort ob = new InsertionSort(); 
+        ob.insertionSort(arr); 
+  
+        printArray(arr); 
+    } 
+}
